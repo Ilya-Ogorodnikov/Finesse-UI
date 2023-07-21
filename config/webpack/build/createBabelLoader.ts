086cd5242b2e2391834d53c04654d2ccb4808ea6
Ilya <ogorodnikovia@answer-42.ru>
2023-07-21 20:27:17 +0300

    add comments in eslintrrc and babel

--- config/webpack/build/createBabelLoader.ts
+++ config/webpack/build/createBabelLoader.ts
@@ -6,6 +6,7 @@ interface ICreateBabelLoader extends IBaseConfigOptions {
 
 export const createBabelLoader = (options: ICreateBabelLoader) => {
   const { isDev, isTsx } = options;
+
   // настройка babel для работы с ts и jsx
   return {
     test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
@@ -14,23 +15,33 @@ export const createBabelLoader = (options: ICreateBabelLoader) => {
       loader: 'babel-loader',
       options: {
         presets: [
-          '@babel/preset-env',
-          '@babel/preset-typescript',
+          '@babel/preset-env', // позволяет писать на современном синтаксисе без использования полифилов для старых браузеров
           [
-            '@babel/preset-react',
+            '@babel/preset-typescript', // позволяет работать с TS и парсить jsx
+            {
+              isTsx
+            }
+          ],
+          [
+            '@babel/preset-react', // позволяет работать с React-кодом
             {
               runtime: 'automatic',
             },
           ],
         ],
         plugins: [
-          [
-            '@babel/plugin-transform-typescript',
-            {
-              isTsx,
-            },
-          ],
+          // [
+          //   '@babel/plugin-transform-typescript',
+          //   {
+          //     isTsx,
+          //   },
+          // ],
+
+          // смотрит код на наличие ES6 фич и если они есть, трансформирует код так,
+          // чтобы эти фичи брались не из глобального скоупа, а импортировались из babel-runtime
           '@babel/plugin-transform-runtime',
+
+          // позволяет редактировать компоненты react и не терять их состояния после "горячей перезагрузки"
           isDev && require.resolve('react-refresh/babel'),
         ].filter(Boolean),
       },
